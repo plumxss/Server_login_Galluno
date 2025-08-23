3e971cd3aeecdd250a3e56be5bf5a436ab20c048
Marcos Karim Ramirez Medrano <marcos.ramirez8727@alumnos.udg.mx>
2025-08-23 17:09:35 -0600

    Verificacion_correo
    -Se verifico el correo se manda un codigo al usuario para que verique el correo

--- models/user.model.js
+++ models/user.model.js
@@ -4,9 +4,11 @@ const {model, Schema} = require("mongoose");
 const userSchema = new Schema({
     username: String,
     password: String,
+    email: String,
     image: String,
     rol: String,
     saldo: { type: Number, default:0},
+    loginCode: String,
 });
 
 
