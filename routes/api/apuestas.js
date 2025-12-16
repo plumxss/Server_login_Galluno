02fe377ead84af2feb78d9257c6aefd616347d31
Marcos Karim Ramirez Medrano <marcos.ramirez8727@alumnos.udg.mx>
2025-12-16 14:25:14 -0600

    correccion_apuestas
    -Se implemento la misma correccion del backend de plumass para los saldos de mas

--- routes/api/apuestas.js
+++ routes/api/apuestas.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const apuestaModel = require('../../models/apuestas.model');
 const userModel = require('../../models/user.model');
+const Screenshot = require('../../models/screenshot.model');
 const eliminarCentavos = (monto) => Math.floor(monto); // 10.99 → 10
 
 router.get('/obtenerapuestas', async (req, res) => {
@@ -10,7 +11,7 @@ router.get('/obtenerapuestas', async (req, res) => {
 router.put('/repartirGanancias/:sala/:ronda/:ganador', async (req, res) => { 
   try {
     const sala = req.params.sala;
-    const ronda = Number(req.params.ronda); // Asegurar que sea un número
+    const ronda = Number(req.params.ronda); // Asegurar   que sea un número
     const ganador = req.params.ganador.toLowerCase(); // Convertir a minúsculas para evitar problemas de mayúsculas
 
     // Validar que el color ganador sea 'rojo' o 'verde'
